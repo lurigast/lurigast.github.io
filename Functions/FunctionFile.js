de90333541cc07512b1fc7ae594c80fcef0bd6d6
lurigast <o.santesson@gmail.com>
2023-06-08 00:14:38 +0200

    cv2

--- Functions/FunctionFile.js
+++ Functions/FunctionFile.js
@@ -9,17 +9,20 @@ Office.onReady(() => {
 function BuildXMLRequestForRoomName(roomName) {
 
     var result =
-'<?xml version="1.0" encoding="utf-8"?>' +
-'<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">' +
-'  <soap:Header>' +
-'    <RequestServerVersion Version="Exchange2013" xmlns="http://schemas.microsoft.com/exchange/services/2006/types" soap:mustUnderstand="0" />' +
-'  </soap:Header>' +
-'  <soap:Body>' +
-'      <ResolveNames xmlns="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" ReturnFullContactData="true">' +
-'      <UnresolvedEntry>' + roomName + '</UnresolvedEntry>' +
-'    </ResolveNames>' +
-'  </soap:Body>' +
-'</soap:Envelope>';
+        `<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">
+<soap:Header>
+<RequestServerVersion Version="Exchange2013" xmlns="http://schemas.microsoft.com/exchange/services/2006/types" soap:mustUnderstand="0" />
+</soap:Header>
+<soap:Body>
+    <ResolveNames xmlns="http://schemas.microsoft.com/exchange/services/2006/messages"
+                  xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types"
+                  ReturnFullContactData="true">
+      <UnresolvedEntry>`+ roomName + `</UnresolvedEntry>
+    </ResolveNames>
+  </soap:Body>
+</soap:Envelope>`
+    return result;
 
 //    var result = `<?xml version="1.0" encoding="utf-8"?>
 //<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">
