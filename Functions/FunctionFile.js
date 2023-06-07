8926b70fb00d1ff59185822daab54947b7f0e37a
lurigast <o.santesson@gmail.com>
2023-06-08 00:02:53 +0200

    cn3

--- Functions/FunctionFile.js
+++ Functions/FunctionFile.js
@@ -8,7 +8,7 @@ Office.onReady(() => {
 
 function BuildXMLRequestForRoomName(roomName) {
 
-/*    var result =
+    var result =
 '<?xml version="1.0" encoding="utf-8"?>' +
 '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">'
 '  <soap:Header>' +
@@ -19,19 +19,20 @@ function BuildXMLRequestForRoomName(roomName) {
 '      <UnresolvedEntry>' + roomName + '</UnresolvedEntry>' +
 '    </ResolveNames>' +
 '  </soap:Body>' +
-'</soap:Envelope>';*/
+'</soap:Envelope>';
 
-    var result = `<?xml version="1.0" encoding="utf-8"?>
-    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">
-<soap:Header>
-<RequestServerVersion Version="Exchange2013" xmlns="http://schemas.microsoft.com/exchange/services/2006/types" soap:mustUnderstand="0" />
-</soap:Header>
-<soap:Body>
-    <ResolveNames xmlns="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" ReturnFullContactData="true">
-      <UnresolvedEntry>`+ roomName + `</UnresolvedEntry>
-    </ResolveNames>
-  </soap:Body>
-</soap:Envelope>`;
+//    var result = `<?xml version="1.0" encoding="utf-8"?>
+//<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">
+//<soap:Header>
+//<RequestServerVersion Version="Exchange2013" xmlns="http://schemas.microsoft.com/exchange/services/2006/types" soap:mustUnderstand="0" />
+//</soap:Header>
+//<soap:Body>
+//<ResolveNames xmlns="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" ReturnFullContactData="true">
+//<UnresolvedEntry>`+ roomName + `</UnresolvedEntry>
+//</ResolveNames>
+//</soap:Body>
+//</soap:Envelope>`;
+    console.log(result);
     return result;
 };
 
