8026d1f20fb2655bdc89a0d506b1ca35858738e3
lurigast <o.santesson@gmail.com>
2023-06-08 22:56:10 +0200

    cm

--- Functions/FunctionFile.js
+++ Functions/FunctionFile.js
@@ -67,7 +67,7 @@ function SetLocationToAppointmentBody(LocationToBody) {
         console.log("bodyFormat: " + bodyFormat);
     });
 
-    Office.context.mailbox.item.body.prependAsync(LocationToBody, { coercionType: bodyFormat }, (asyncResult) => {
+    Office.context.mailbox.item.body.prependAsync(LocationToBody, { coercionType: Office.CoercionType.Html }, (asyncResult) => {
         if (asyncResult.status === Office.AsyncResultStatus.Failed) {
             console.log("Action failed with error: " + asyncResult.error.message);
             return;
